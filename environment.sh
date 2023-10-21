38400600c5428431a8791367b15b7b89e570f095
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-10-21 15:53:35 -0300

    ✨ (bash): Environment
    add script for create alias for some applications

---
+++ environment.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# terminator
+ln -s ~/dotfiles/config/terminator/ ~/.config/terminator
+
+# alacritty
+ln -s ~/dotfiles/config/alacritty/ ~/.config/alacritty
+
+# gitmoji-nodejs
+ln -s ~/dotfiles/config/gitmoji-nodejs/ ~/.config/gitmoji-nodejs
+
+# sublime
+#ln -s ~/dotfiles/config/sublime/Packages/User/ ~/.config/sublime-text-4/Packages/User
+
+# git
+ln -s ~/dotfiles/config/gitconfig/.gitconfig ~/.gitconfig
+
+# Powerlevel10k
+ln -s ~/dotfiles/config/zsh/.p10k.zsh ~/.p10k.zsh
+
+# ZSH
+rm ~/.zshrc
+ln -s ~/dotfiles/config/zsh/.zshrc ~/.zshrc
+
+# LunarVim
+ln -s ~/dotfiles/config/lvim/config.lua ~/.config/lvim/config.lua
+
+# Tmux
+ln -s ~/dotfiles/config/tmux/.tmux.conf ~/.tmux.conf
+
+#Terraform
+# if [ -f '~/.terraformrc' ]; then mv ~/.terraformrc ~/.terraformrc_original; fi
+# ln -s ~/dotfiles/config/.terraformrc ~/.terraformrc
+
