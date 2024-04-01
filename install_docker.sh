55622eaf5570acc981e1f339761ec57fcbd1152c
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2024-04-01 09:44:06 -0300

    ♻️ (bash): Change link for install docker

--- install_docker.sh
+++ install_docker.sh
@@ -15,15 +15,23 @@ echo;
 echo "Install Pre-requisites"
 
 (
-  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && \
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update && \
+  sudo apt-get install ca-certificates curl && \
+  sudo install -m 0755 -d /etc/apt/keyrings && \
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && \
+  sudo chmod a+r /etc/apt/keyrings/docker.asc && \
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 ) &> /dev/null && echo -e "$green UPDATE OK $endcolor" || echo -e "$red UPDATE FAILED $endcolor"
 
 echo;
 echo "Install Docker"
 (
-  sudo apt install -y docker-ce
+  sudo apt update && \
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 ) &> /dev/null && echo -e "$green UPDATE OK $endcolor" || echo -e "$red UPDATE FAILED $endcolor"
 
+newgrp docker
 sudo usermod -aG docker $USER
