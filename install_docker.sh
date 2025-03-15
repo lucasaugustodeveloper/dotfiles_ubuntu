a296976ebb1a9d46a7d305050c729a7d3e956697
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2025-03-15 18:35:53 -0300

    ♻️ Change variable

--- install_docker.sh
+++ install_docker.sh
@@ -22,8 +22,8 @@ echo "Install Pre-requisites"
   sudo chmod a+r /etc/apt/keyrings/docker.asc && \
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 ) &> /dev/null && echo -e "$green UPDATE OK $endcolor" || echo -e "$red UPDATE FAILED $endcolor"
 
 echo;
