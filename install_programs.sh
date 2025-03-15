7223a14a3a3467d373e21e0588e7395445b30bda
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2025-03-15 18:32:03 -0300

    ♻️ Remove some programs not found and remove gh install

--- install_programs.sh
+++ install_programs.sh
@@ -14,7 +14,7 @@ fi
 echo ;
 echo "INSTALLATION OTHER PACKAGES"
 (
-	sudo apt install -y libindicator3-7 libappindicator1 libindicator-dev && \
+	sudo apt install -y libindicator3-7 libindicator-dev && \
 		libindicator3-dev libindicator7 libindicator3-tools && \
 		libnss3-tools fish zsh htop unzip ssh openssh-client && \
 		software-properties-common
@@ -23,9 +23,7 @@ echo "INSTALLATION OTHER PACKAGES"
 echo ;
 echo "INSTALLATION DEVELOPMENT KIT"
 (
-	sudo apt install -y git zlib1g-dev libssl-dev libreadline-dev libyaml-dev && \
-		libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl3 && \
-		libcurl4-openssl-dev libffi-dev
+	sudo apt install -y git zlib1g-dev libssl-dev libreadline-dev libyaml-dev
 ) &> /dev/null && echo -e "$green UPDATE OK $endcolor" || echo -e "$red UPDATE FAILED $endcolor"
 
 echo ;
@@ -37,15 +35,3 @@ echo "INSTALLATION KVM"
 		bridge-utils && \
 		cpu-checker
 ) &> /dev/null && echo -e "$green UPDATE OK $endcolor" || echo -e "$red UPDATE FAILED $endcolor"
-
-echo;
-echo "Github CLI"
-(
-  type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-  && sudo apt update \
-  && sudo apt install gh -y
-) &> /dev/null && echo -e "$green UPDATE OK $endcolor" || echo -e "$red UPDATE FAILED $endcolor"
-
